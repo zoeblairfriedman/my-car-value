35ee8eaa8dac77bb6142cc31471223efd36afd8b
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-12-22 14:05:47 -0500

    adds logic to service methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -10,8 +10,29 @@ export class UsersService {
   }
 
   create(email: string, password: string){
+    // hooks only run with an INSTANCE of an entity versus an object. save and remove run hooks if there's an instance
     const user = this.repo.create({email,password})
 
     return this.repo.save(user)
   }
+
+  findOne(id: number){
+    // could also be search criteria
+    return this.repo.findOne(id);
+  }
+
+  find(email: string){
+    return this.repo.find({email: email})
+  }
+
+  async update(id: number, attrs: Partial<User>){
+    const user = await this.findOne(id)
+    if (!user){
+      throw new Error('user not found')
+    }
+    Object.assign(user, attrs)
+    return this.repo.save(user)
+  }
+
+  remove(){}
 }
